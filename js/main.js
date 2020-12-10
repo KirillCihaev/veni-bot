94150d0874e3f02fbf9a8f13dc9e546c764cbfb6
KirillCihaev <73433907+KirillCihaev@users.noreply.github.com>
2020-12-10 16:12:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-//123//
+let headerButtons = document.querySelector(".header-buttons");
+let sun = document.querySelector(".icon-sun");
+let moon = document.querySelector(".icon-moon");
+let whiteTheme = document.querySelector(".body");
+let blackTheme = document.querySelector(".body");
+
+headerButtons.addEventListener('click', function (event) {
+    event.preventDefault();
+    sun.classList.toggle('not-visible');
+    moon.classList.toggle('not-visible');
+    whiteTheme.classList.toggle('white');
+    blackTheme.classList.toggle('black');
+});
