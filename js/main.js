d67a6c96f340f1df1d68f2fdd8ec21a04233489e
KirillCihaev <73433907+KirillCihaev@users.noreply.github.com>
2020-12-16 13:02:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 let headerButtons = document.querySelector(".header-buttons");
 let sun = document.querySelector(".icon-sun");
 let moon = document.querySelector(".icon-moon");
-let whiteTheme = document.querySelector(".body");
-let blackTheme = document.querySelector(".body");
+let whiteThemeBody = document.querySelector(".body");
+let blackThemeBody = document.querySelector(".body");
 let whiteThemeOpportunities = document.querySelector(".section-opportunities");
 let blackThemeOpportunities = document.querySelector(".section-opportunities");
 let whiteThemePremium = document.querySelector(".section-premium");
 let blackThemePremium = document.querySelector(".section-premium");
+let whiteThemeFooter = document.querySelector(".footer");
+let blackThemeFooter = document.querySelector(".footer");
 
 headerButtons.addEventListener('click', function (event) {
     event.preventDefault();
     sun.classList.toggle('not-visible');
     moon.classList.toggle('not-visible');
-    whiteTheme.classList.toggle('white');
-    blackTheme.classList.toggle('black');
+    whiteThemeBody.classList.toggle('white');
+    blackThemeBody.classList.toggle('black');
     whiteThemeOpportunities.classList.toggle('white');
     blackThemeOpportunities.classList.toggle('black');
     whiteThemePremium.classList.toggle('white');
     blackThemePremium.classList.toggle('black');
+    whiteThemeFooter.classList.toggle('white');
+    blackThemeFooter.classList.toggle('black');
 });
